9575594259368151b2ce601b0879f7b849fd622b
Andrey Lesnikov <ozkriff@gmail.com>
2015-11-23 14:47:44 +0300

    Added 'archives', 'tags' and 'about' links

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,15 +15,11 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# # Blogroll
-# LINKS =  (('Pelican', 'http://getpelican.com/'),
-#           ('Python.org', 'http://python.org/'),
-#           ('Jinja2', 'http://jinja.pocoo.org/'),
-#           ('You can modify those links in your config file', '#'),)
-
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
+SECTIONS = [
+    (u'Архив', 'archives.html'),
+    (u'Тэги', 'tags.html'),
+    (u'О блоге', 'pages/about.html')
+]
 
 DEFAULT_PAGINATION = 10
 
