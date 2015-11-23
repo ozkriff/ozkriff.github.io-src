502c28b1726308b44b9f823f766d929e284bf52f
Andrey Lesnikov <ozkriff@gmail.com>
2015-11-24 01:04:00 +0300

    Fixed atom feeds, added rss feeds

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,13 @@ from pelicanconf import *
 SITEURL = 'http://ozkriff.github.io'
 RELATIVE_URLS = False
 
+FEED_ATOM = 'feeds/atom.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TAG_FEED_ATOM = 'feeds/%s.atom.xml'
+
+FEED_RSS = 'feeds/rss.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+TAG_FEED_RSS = 'feeds/%s.rss.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
